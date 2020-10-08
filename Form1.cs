3ca6d9130426889f3e6ef1470f60ce769ca06bf9
Vladimir Kotov <ivelostmymind8@gmail.com>
2020-10-08 18:36:33 +0300

    reading tags to textboxes and dgv

--- Form1.cs
+++ Form1.cs
@@ -13,23 +13,16 @@ namespace mp3_editor
     public partial class Form1 : Form
     {
         List<String> filenames = new List<string>();
-        private String tfile, title, artist, album, genre, year;
-
         
-
         public Form1()
         {
             InitializeComponent();
         }
-
-
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
             Close();
         }
-
-        public void openToolStripMenuItem_Click(object sender, EventArgs e)
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {      
             if (ofd.ShowDialog() == DialogResult.Cancel)
             {
@@ -42,34 +35,22 @@ namespace mp3_editor
                 {
                     filenames.Add(filepath);
                     i++;
-                    dataGridView1.Rows.Add(filepath);
+                    dataGridView1.Rows.Add(String.Concat(TagLib.File.Create(filepath).Tag.Performers), 
+                        TagLib.File.Create(filepath).Tag.Title,
+                        TagLib.File.Create(filepath).Tag.Album);
                 }
+                toolStripStatusLabel1.Text = i.ToString();
             }            
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        // private void addList()
-        // {
-        //     int i = 0;
-        //     foreach (String filepath in ofd.FileNames)
-        //     {
-        //         filenames.Add(filepath);
-        //         i++;
-        //         dataGridView1.Rows.Add(filepath);
-        //         var rowi = dataGridView1.CurrentCell.RowIndex;
-        //         
-        //         
-        //     }
-        //     toolStripStatusLabel1.Text = i.ToString();
-        // }
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             var rowi = dataGridView1.CurrentCell.RowIndex;
             parsemp3 parce = new parsemp3();
-            var tb = parce.readMusic(filenames[rowi]);
-            tb_title.Text = tb;
+            tb_artist.Text = parce.getArtist(filenames[rowi]);
+            tb_genre.Text = parce.getGenre(filenames[rowi]);
+            tb_title.Text = parce.getTitle(filenames[rowi]);
+            tb_album.Text = parce.getAlbum(filenames[rowi]);
+            tb_year.Text = parce.getYear(filenames[rowi]);
         }
     }
 }
--- parsemp3.cs
+++ parsemp3.cs
@@ -1,18 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using TagLib;
 
 namespace mp3_editor
 {
     public class parsemp3
     {
-        
-        public string readMusic(String filepath)
+        public string getTitle(String filepath)
         {
-            var tfile = TagLib.File.Create(filepath);
             String title;
+            var tfile = TagLib.File.Create(filepath);
             title = tfile.Tag.Title;
             return title;
         }
-
+        public string getAlbum(String filepath)
+        {
+            String album;
+            var tfile = TagLib.File.Create(filepath);
+            album = tfile.Tag.Album;
+            return album;
+        }
+        public string getYear(String filepath)
+        {
+            String year;
+            var tfile = TagLib.File.Create(filepath);
+            year = Convert.ToString(tfile.Tag.Year);
+            return year;
+        }
+        public string getArtist(String filepath)
+        {
+            String[] art;
+            string Artists;
+            var tfile = TagLib.File.Create(filepath);
+            art = tfile.Tag.Performers;
+            Artists = String.Concat(art);
+            return Artists;
+        }
+        public string getGenre(String filepath)
+        {
+            String[] gen;
+            string Genres;
+            var tfile = TagLib.File.Create(filepath);
+            gen = tfile.Tag.Genres;
+            Genres = String.Concat(gen);
+            return Genres;
+        }
     }
 }
